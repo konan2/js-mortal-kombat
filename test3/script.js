d44aabaa69195de1962637dcb676090e290987d1
Oleg Bagmet <o.bagmet@plarium.com>
2017-01-13 17:40:44 +0200

    Level toggle added

--- test3/script.js
+++ test3/script.js
@@ -24,28 +24,50 @@ let playerTwoKeycodes = {
   101: "makeBlock"
 }
 
-var cyrax = {
+let cyrax = {
   name: "cyrax",
   soundName: new Audio('audio/names/cyrax.mp3'),
   soundRun: new Audio('audio/syborg_run.mp3'),
   previewImgIcon: "img/players-list/cyrax.gif",
   previewImg: "img/players-list/versus/cyrax.png",
+  playerPosX: 0,
+  playerPosY: 0,
+  playerWidth: 150,
+  playerHeight: 262,
   speed: 3,
   handKickDamage: 2,
   footKickDamage: 4,
   blockedDamage: 1
 }
 
-var kabal = {
+let kabal = {
   name: "kabal",
   soundName: new Audio('audio/names/kabal.mp3'),
   soundRun: new Audio('audio/syborg_run.mp3'),
   previewImgIcon: "img/players-list/kabal.gif",
   previewImg: "img/players-list/versus/kabal.png",
+  playerPosX: 0,
+  playerPosY: 0,
+  playerWidth: 150,
+  playerHeight: 262,
   speed: 2,
   handKickDamage: 3,
   footKickDamage: 5,
   blockedDamage: 2
+}
+
+const gameData = {
+  roundDuration: 20,
+  roundTime: undefined,
+  roundsMaxCount: 3,
+  currentRound: 0,
+  musicEnabled: true,
+  soundsEnabled: true,
+  playerOneChoosen: false,
+  playerTwoChoosen: false,
+  playersAreChoosen: false,
+  roundStarted: false,
+  roundEnded: false
 }
 
 const defaultControls = {
@@ -61,10 +83,6 @@ const defaultControls = {
 
 const defautlPlayerData = {
   life: 100,
-  playerPosX: 0,
-  playerPosY: 0,
-  playerWidth: 150,
-  playerHeight: 262,
   keyPressedJump: false,
   footKickEnd: true,
   handKickEnd: true,
@@ -74,13 +92,165 @@ const defautlPlayerData = {
   isDamaged: false,
   defeated: false,
   canRun: true,
-  speed: 1,
-  handKickDamage: 1,
-  footKickDamage: 1,
-  blockedDamage: 1,
   keycodes: undefined,
   controls: {}
 };
+
+
+
+
+
+
+
+
+
+
+// Arenas
+
+const defaultArena = {
+  width: '100%',
+  height: '100%',
+  selector: document.querySelector("#arena"),
+  skySelector: document.querySelector(".arena-decor__item_sky"),
+  decorSelector: document.querySelector(".arena-decor__item_decor"),
+  groundSelector: document.querySelector(".arena-decor__item_ground")
+}
+
+
+
+
+let temple = {
+  name: 'temple',
+  sky: 'img/levels/temple/temple-bg.jpg',
+  decor: 'img/levels/temple/temple-decor.png',
+  ground: 'img/levels/temple/temple-floor.png'
+}
+
+let church = {
+  name: 'church',
+  sky: 'img/levels/church/church-bg.png',
+  ground: 'img/levels/church/church-floor.png'
+}
+
+let shaoKahnPlace = {
+  name: 'shaoKahnArena',
+  sky: 'img/levels/arena/arena-bg.jpg'
+}
+
+let plant = {
+  name: 'plant',
+  sky: 'img/levels/plant/plant-bg.jpg',
+  decor: 'img/levels/plant/floor-decor.png',
+  ground: 'img/levels/plant/plant-floor.jpg'
+}
+
+let spaceship = {
+  name: 'spaceship',
+  sky: 'img/levels/spaceship/spaceship-bg.png',
+  clouds: 'img/levels/spaceship/space-sky.jpg',
+  decor: 'img/levels/plant/floor-decor.png',
+  ground: 'img/levels/plant/plant-floor.jpg'
+}
+
+let vulkano = {
+  name: 'vulkano',
+  sky: 'img/levels/vulkano/vulkano-bg.png'
+}
+
+let bunker = {
+  name: 'bunker',
+  sky: 'img/levels/bunker/bunker-bg.jpg'
+}
+
+let mars = {
+  name: 'mars',
+  sky: 'img/levels/mars/mars-bg.png',
+  clouds: 'img/levels/desert/clouds3.png',
+  decor: 'img/levels/mars/2.jpg'
+}
+
+let desert = {
+  name: 'desert',
+  sky: 'img/levels/desert/8.jpg',
+  clouds: 'img/levels/desert/clouds3.png',
+  decor: 'img/levels/desert/p_2.png',
+  ground: 'img/levels/desert/p_1.gif'
+}
+
+
+let portal = {
+  name: 'portal',
+  sky: 'img/levels/portal/sky-portal.jpg',
+  clouds: 'img/levels/portal/portal-cloud.png',
+  ground: 'img/levels/portal/p_1.png'
+}
+
+
+let allArenas = [temple, church, shaoKahnPlace, plant, spaceship, vulkano, bunker, mars, desert, portal];
+let allArenasSize = allArenas.length;
+
+class arena{
+  constructor(){
+    this.arenaCount = 0;
+    this.setArena(0);
+  }
+  setArena(index){
+    this.arenaData = Object.assign({}, defaultArena, allArenas[index]);
+    this.arenaData.selector.classList = allArenas[index].name;
+    this.arenaData.skySelector.setAttribute("style", "background: url(" + this.arenaData.sky + ");");
+    if(this.arenaData.decor){
+      this.arenaData.decorSelector.setAttribute("style", "background: url(" + this.arenaData.decor + ");");
+    }
+    else{
+      this.arenaData.decorSelector.removeAttribute("style");
+    }
+    if(this.arenaData.ground){
+      this.arenaData.groundSelector.setAttribute("style", "background: url(" + this.arenaData.ground + ");");
+    }
+    else{
+      this.arenaData.decorSelector.removeAttribute("style");
+    }
+    //this.setArenaDecorStyles(allArenas[index]);
+  }
+  // setArenaDecorStyles(object){
+  //   for (var property in object) {
+  //     console.log(property)
+  //     this.arenaData.skySelector.setAttribute("style", "background: url(" + object[property] + ");");
+  //   }
+  // }
+  nextArena(){
+    this.arenaCount++;
+    if (this.arenaCount >= allArenasSize){
+      this.arenaCount = 0;
+    }
+    this.setArena(this.arenaCount);
+  }
+  prevArena(){
+    this.arenaCount--;
+    if (this.arenaCount < 0){
+      this.arenaCount = 9;
+    }
+    this.setArena(this.arenaCount);
+  }
+  randomArena(){
+    this.setArena(RandomValue(allArenasSize));
+  }
+}
+
+let currentArena = new arena();
+
+// Utility functions
+
+function RandomValue(max){
+  return Math.floor(Math.random() * max);
+}
+
+
+
+
+
+
+
 
 class player {
   constructor(params) {
@@ -95,15 +265,12 @@ class player {
 
   doAction(eventCode, eventType){
     var AllkeyCodes = Object.keys(this.playerData.keycodes);
-
     if(AllkeyCodes.includes(eventCode.toString())){
       let actionName = this.playerData.keycodes[eventCode];
       if(eventType === "keydown") {
         if(!this.playerData.controls[actionName]){
           this.playerData.controls[actionName] = true;
         }
-
-        //this[actionName]();
       }
       if(eventType === "keyup"){
         this.playerData.controls[actionName] = false;
@@ -124,16 +291,16 @@ class player {
     this.playerData.playerSelector.style[direction] = parseInt(this.playerData.playerSelector.style[direction]) - speed + 'px';
   }
   moveForward(){
-    this.movePlayer("right", 1);
+    this.movePlayer("right", this.playerData.speed);
   }
   moveBackward(){
-    this.movePlayer("left", 1);
+    this.movePlayer("left", this.playerData.speed);
   }
   moveTop(){
-    this.movePlayer("top", 1);
+    this.movePlayer("top", this.playerData.speed);
   }
   moveDown(){
-    this.movePlayer("bottom", 1);
+    this.movePlayer("bottom", this.playerData.speed);
   }
   kickHand(){
     console.log("Hand kick");
@@ -149,13 +316,13 @@ class player {
   }
 }
 
-// создать игрока 1
+// Create player 1
 let playerOne = new player(cyrax);
 playerOne.playerData.keycodes = playerOneKeycodes;
 playerOne.playerData.playerSelector = document.querySelector("#player-one");
 
 
-// создать игрока 2
+// Create player 2
 let playerTwo = new player(kabal);
 playerTwo.playerData.keycodes = playerTwoKeycodes;
 playerTwo.playerData.playerSelector = document.querySelector("#player-two");
